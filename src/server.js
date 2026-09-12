534a66fdfd3f01daff1484c1c479d49665c21eb5
Rdazochka <rdazochka@gmail.com>
2026-09-12 21:36:42 +0300

    fix notes routes import

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 
-import { notesRoutes } from './routes/notesRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
