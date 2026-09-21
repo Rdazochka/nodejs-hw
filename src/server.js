42535143c2642dd02efb29f2efbb403fed89759e
Rdazochka <rdazochka@gmail.com>
2026-09-21 21:13:22 +0300

    done all

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
+
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -16,6 +18,8 @@ app.use(express.json());
 app.use(logger);
 
 app.use(notesRoutes);
+
+app.use(errors());
 
 app.use(notFoundHandler);
 
